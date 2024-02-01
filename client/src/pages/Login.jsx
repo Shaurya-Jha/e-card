26176888ad4989fc77717883cf74d96f19996631
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-01 15:46:25 +0530

    login page completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,63 @@
+import { useForm } from "react-hook-form";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Alert,
+  AlertIcon,
+  Button,
+  Container,
+} from "@chakra-ui/react";
 
 const LoginPage = () => {
-    return ( 
-        <div>
-            Login page
-        </div>
-     );
-}
- 
+  // properties of react-hook-form for form handling
+  // initialized the useForm library
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // form onsubmit handler
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <Container>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* username field */}
+          <FormControl className="mt-2">
+            <FormLabel>Username</FormLabel>
+            <Input {...register("userName", { required: true })} />
+            {errors.userName && (
+              <Alert className="mt-1" status="error" variant="left-accent">
+                <AlertIcon />
+                Username is required...
+              </Alert>
+            )}
+          </FormControl>
+
+          {/* password field */}
+          <FormControl className="mt-2">
+            <FormLabel>Password</FormLabel>
+            <Input {...register("userPassword", { required: true })} />
+            {errors.userPassword && (
+              <Alert className="mt-1" status="error" variant="left-accent">
+                <AlertIcon />
+                Password is required...
+              </Alert>
+            )}
+          </FormControl>
+
+          <FormControl className="flex justify-center mt-4">
+            <Button type="submit">Log-in</Button>
+          </FormControl>
+        </form>
+      </Container>
+    </div>
+  );
+};
+
 export default LoginPage;
