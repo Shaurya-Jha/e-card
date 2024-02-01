72ba3cdbc60dbbeaac1329369e7ba061f2294c33
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-01 17:34:38 +0530

    Update InfoForm.jsx

--- client/src/pages/user-dashboard/InfoForm.jsx
+++ client/src/pages/user-dashboard/InfoForm.jsx
@@ -18,6 +18,7 @@ const InfoForm = () => {
           <Image
             boxSize={250}
             objectFit={"contain"}
+            borderRadius='full'
             src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             alt="image"
           />
