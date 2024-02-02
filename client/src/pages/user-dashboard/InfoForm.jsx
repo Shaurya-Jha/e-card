e5f1062a59c56c5c37c664b1777133feddab6016
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-02 11:41:33 +0530

    added new input fields for data

--- client/src/pages/user-dashboard/InfoForm.jsx
+++ client/src/pages/user-dashboard/InfoForm.jsx
@@ -7,13 +7,15 @@ import {
   Text,
   Button,
   FormControl,
+  Textarea,
+  Radio, RadioGroup
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const InfoForm = () => {
   return (
     <div>
-      <Container>
+      <Container maxW='container.sm' className="border border-gray-200 p-4 bg-gray-100/50">
         <div className="flex justify-center items-center my-4 pb-4">
           {/* <Image src='https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='image' height={400} width={400} /> */}
           {/* <Box boxSize="sm"> */}
@@ -36,7 +38,7 @@ const InfoForm = () => {
           <FormControl>
             <HStack spacing={2}>
               <Text className="underline decoration-green-500 underline-offset-4">
-                Name:
+                First Name
               </Text>
               <Input contentEditable="false" value="your name" />
             </HStack>
@@ -44,28 +46,91 @@ const InfoForm = () => {
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Number:</Text>
+              <Text className="underline">Last Name</Text>
               <Input contentEditable="false" value="your number" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Email:</Text>
+              <Text className="underline">Email</Text>
               <Input contentEditable="false" value="your email" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Age:</Text>
+              <Text className="underline">Age</Text>
               <Input contentEditable="false" value="your age" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Date of Birth:</Text>
+              <Text className="underline">Phone Number</Text>
+              <Input contentEditable="false" value="your date of birth" />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Emergency Contact 1</Text>
+              <Input contentEditable="false" value="your date of birth" />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Emergency Contact 2</Text>
+              <Input contentEditable="false" value="your date of birth" />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Address</Text>
+              {/* <Input contentEditable="false" value="your date of birth" /> */}
+              <Textarea resize={"none"} contentEditable="false" value="some pre-fetched data will be shown here in this textarea field. you have to just verify"  />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Blood Group</Text>
+              <Input contentEditable="false" value="your blood group" />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Allergy</Text>
+              <Input contentEditable="false" value="your date of birth" />
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Medical Insurance</Text>
+              <RadioGroup>
+                <Radio value="yes">Yes</Radio>
+                <Radio value="no">No</Radio>
+              </RadioGroup>
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Any Surgery</Text>
+              <RadioGroup>
+                <Radio value="yes">Yes</Radio>
+                <Radio value="no">No</Radio>
+              </RadioGroup>
+            </HStack>
+          </FormControl>
+
+          <FormControl>
+            <HStack spacing={2}>
+              <Text className="underline">Running Medicine</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
