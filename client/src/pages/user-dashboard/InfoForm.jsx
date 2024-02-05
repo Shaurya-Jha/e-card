17ffb40fa6418f9b37bd2f0089d247f8ebe3a508
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-05 10:59:55 +0530

    adjusting proper layout for info form

--- client/src/pages/user-dashboard/InfoForm.jsx
+++ client/src/pages/user-dashboard/InfoForm.jsx
@@ -8,7 +8,11 @@ import {
   Button,
   FormControl,
   Textarea,
-  Radio, RadioGroup
+  Radio,
+  RadioGroup,
+  Grid,
+  GridItem,
+  Flex,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 // edit icon
@@ -17,10 +21,12 @@ import { FaEdit } from "react-icons/fa";
 const InfoForm = () => {
   return (
     <div>
-      <Container maxW='container.sm' className="border border-gray-200 p-4 bg-gray-100/50">
+      <Container
+        // maxW="container.sm"
+        maxW={"3xl"}
+        className="border border-gray-200 p-4 bg-gray-100/50"
+      >
         <div className="flex justify-center items-center my-4 pb-4">
-          {/* <Image src='https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='image' height={400} width={400} /> */}
-          {/* <Box boxSize="sm"> */}
           <Image
             boxSize={250}
             objectFit={"contain"}
@@ -28,18 +34,18 @@ const InfoForm = () => {
             src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             alt="image"
           />
-          {/* </Box> */}
         </div>
 
         {/* form data fields */}
         {/* <div className="border border-gray-300 p-4 rounded-3xl"> */}
         {/* form data fields */}
-        {/* <form> */}
+        <form>
+
         <VStack spacing={4}>
-          {/* fields for the data to be edited */}
-          <FormControl>
+        {/* fields for the data to be edited */}
+        <FormControl>
             <HStack spacing={2}>
-              <Text className="underline decoration-green-500 underline-offset-4">
+              <Text className="underline decoration-green-500 underline-offset-4 w-[200px]">
                 First Name
               </Text>
               <Input contentEditable="false" value="your name" />
@@ -48,71 +54,74 @@ const InfoForm = () => {
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Last Name</Text>
+              <Text className="underline w-[200px]">Last Name</Text>
               <Input contentEditable="false" value="your number" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Email</Text>
+              <Text className="underline w-[200px]">Email</Text>
               <Input contentEditable="false" value="your email" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Age</Text>
+              <Text className="underline w-[200px]">Age</Text>
               <Input contentEditable="false" value="your age" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Phone Number</Text>
+              <Text className="underline w-[200px]">Phone Number</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Emergency Contact 1</Text>
+              <Text className="underline w-[200px]">Emergency Contact 1</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Emergency Contact 2</Text>
+              <Text className="underline w-[200px]">Emergency Contact 2</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Address</Text>
-              {/* <Input contentEditable="false" value="your date of birth" /> */}
-              <Textarea resize={"none"} contentEditable="false" value="some pre-fetched data will be shown here in this textarea field. you have to just verify"  />
+              <Text className="underline w-[200px]">Address</Text>
+              <Textarea
+                resize={"none"}
+                contentEditable="false"
+                value="some pre-fetched data will be shown here in this textarea field. you have to just verify"
+              />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Blood Group</Text>
+              <Text className="underline w-[200px]">Blood Group</Text>
               <Input contentEditable="false" value="your blood group" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Allergy</Text>
+              <Text className="underline w-[200px]">Allergy</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Medical Insurance</Text>
+              <Text className="underline w-[200px]">Medical Insurance</Text>
               <RadioGroup>
                 <Radio value="yes">Yes</Radio>
                 <Radio value="no">No</Radio>
@@ -122,7 +131,7 @@ const InfoForm = () => {
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Any Surgery</Text>
+              <Text className="underline w-[200px]">Any Surgery</Text>
               <RadioGroup>
                 <Radio value="yes">Yes</Radio>
                 <Radio value="no">No</Radio>
@@ -132,46 +141,19 @@ const InfoForm = () => {
 
           <FormControl>
             <HStack spacing={2}>
-              <Text className="underline">Running Medicine</Text>
+              <Text className="underline w-[200px]">Running Medicine</Text>
               <Input contentEditable="false" value="your date of birth" />
             </HStack>
           </FormControl>
 
-          <Link to="/user-dashboard/edit-form">
+        <Link to="/user-dashboard/edit-form">
             <Button marginTop={5} backgroundColor={"green"} color={"white"}>
-              <FaEdit className="mr-2" />Edit
+              <FaEdit className="mr-2" />
+              Edit
             </Button>
-          </Link>
+          </Link> 
         </VStack>
-        {/* </form> */}
-
-        {/* <VStack alignContent={'center'}>
-            <HStack spacing={2}>
-              <Text>Name:</Text>
-              <Input contentEditable="false" value={"your name"} />
-            </HStack>
-            <HStack spacing={2}>
-              <Text>Email:</Text>
-              <Input contentEditable="false" value={"your email"} />
-            </HStack>
-
-            <HStack spacing={2}>
-              <Text>Number:</Text>
-              <Input contentEditable="false" value={"your number"} />
-            </HStack>
-
-            <HStack spacing={2}>
-              <Text>Age:</Text>
-              <Input contentEditable="false" value={"your age"} />
-            </HStack>
-
-            <HStack spacing={2}>
-              <Text>Occupation:</Text>
-              <Input contentEditable="false" value={"your occupation"} />
-            </HStack>
-
-            <Button marginTop={5} backgroundColor={'green'} color={'white'}>Edit</Button>
-          </VStack> */}
+        </form>
         {/* </div> */}
       </Container>
     </div>
