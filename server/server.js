2ca9f3576bc2eb8bc7147d80f332e47dae3d3115
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-06 10:53:07 +0530

    database connection established

--- server/server.js
+++ server/server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const cors = require("cors");
+const mongoose = require("mongoose")
 
 // dotenv config
 require('dotenv').config()
-console.log(process.env);       // remove later
+// console.log(process.env.MONGO_URI);       // remove later
 
 app.use(cors())
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`server started running on port: ${PORT}`);
+
+//   console.log(process.env.MONGO_URI);
+
+  await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log(`database connected`);
+  }).catch((err) => {
+    console.log(`Cannot connect to the database: \n\n ${err}`);
+  })
 });
