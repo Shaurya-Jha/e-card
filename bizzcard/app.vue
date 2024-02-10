c6a525746238cda84aafc858d08f7e8bc80a5bbd
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-10 15:25:50 +0530

    basic setup with primevue ui library

--- bizzcard/app.vue
+++ bizzcard/app.vue
@@ -1,5 +1,20 @@
+<script>
+
+import Color from './components/Color.vue';
+
+</script>
+
+<!-- html -->
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h2>Hello world</h2>
+
+    <Color />
   </div>
 </template>
+
+<!-- css -->
+<style>
+/* @import url("primevue/resources/themes/aura-light-green/theme.css"); */
+</style>
--- bizzcard/nuxt.config.ts
+++ bizzcard/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer:{},
+    }
+  },
+
+  // css file
+  css:[
+    `~/assets/css/main.css`,
+    'primevue/resources/themes/aura-light-green/theme.css'
+  ],
+
+  // modules
+  modules: [
+    'nuxt-primevue'
+  ],
+  // primevue config files
+  primevue:{
+
+  }
 })
