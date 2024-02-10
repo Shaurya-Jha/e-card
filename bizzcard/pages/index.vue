63cd90bd86f97e9a611a2be7ea473d9b3c9dd14b
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-10 15:26:12 +0530

    Update index.vue

---
+++ bizzcard/pages/index.vue
@@ -0,0 +1,22 @@
+<!-- html -->
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<!-- javascript -->
+<script>
+
+// primevue components
+import Dialog from 'primevue/dialog'
+
+export default {
+    
+}
+</script>
+
+<!-- css -->
+<style lang="">
+    
+</style>
