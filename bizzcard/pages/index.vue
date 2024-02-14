91e52f2b49cab6b02c8fb87b50884b3fd3616e98
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-14 18:02:34 +0530

    working on file attachment component

--- bizzcard/pages/index.vue
+++ bizzcard/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <!-- <Header /> -->
+        <!-- header and image section -->
+        <div class="md:grid md:grid-cols-2">
+            <div class="px-4 mt-32">
+                <div ref="create" id="step-1" class="pt-8">
+                    <h2 class="font-extrabold text-2xl">Header attachments</h2>
+
+                    <div class="stepC">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
