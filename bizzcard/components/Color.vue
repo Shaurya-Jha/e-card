cdede6e1c8b9b6edc5c9ac1773b9e4c2b74afb75
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-14 15:54:50 +0530

    Update Color.vue

--- bizzcard/components/Color.vue
+++ bizzcard/components/Color.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="mt-6 flex flex-col items-start">
         <div class="flex items-center">
-            <div class="w-12 h-12 rounded mr-3 relative cursor-pointer transition-colors duration-200 focus:outline-none focus:ring ring-gray-200" tabindex="0" :style="{backgroundColor: colors[name].color}" @keydown.space.enter.esc.prevent="colors[name].openPalette = !colors[name].openPalette" @click.self="colors[name].openPalette = !colors[name].openPalette">
+            <div class="w-12 h-12 rounded mr-3 relative cursor-pointer transition-colors duration-200 focus:outline-none focus:ring ring-gray-200"
+                tabindex="0" :style="{ backgroundColor: colors[name].color }"
+                @keydown.space.enter.esc.prevent="colors[name].openPalette = !colors[name].openPalette"
+                @click.self="colors[name].openPalette = !colors[name].openPalette">
                 <transition name="palette">
-                <div class="absolute mt-14 ml-14 z-10" v-show="colors[name].openPalette" v-on-clickaway="() => closeColourPalette(name)">
-                    <colorpicker theme="light" :color="colors[name].color" @changeColour="changeColour($event, name)"></colorpicker>
-                </div>
+                    <div class="absolute mt-14 ml-14 z-10" v-show="colors[name].openPalette"
+                        v-on-clickaway="() => closeColourPalette(name)">
+                        <colorpicker theme="light" :color="colors[name].color" @changeColour="changeColour($event, name)">
+                        </colorpicker>
+                    </div>
                 </transition>
             </div>
 
@@ -26,11 +31,11 @@ export default {
         onClickaway: onClickaway,
     },
 
-    props: ['name','label','colors'],
+    props: ['name', 'label', 'colors'],
 
     methods: {
         // change the color when picked from the colorpicker
-        changeColour({hex}, name){
+        changeColour({ hex }, name) {
             this.colors[name].color = hex
         },
 
@@ -42,5 +47,80 @@ export default {
 }
 </script>
 <style lang="scss">
-    
-</style>
+.palette-enter-active,
+.palette-leave-active {
+    transition: transform 0.2s ease;
+    transform-origin: left top;
+}
+
+.palette-enter,
+.palette-leave-to {
+    transform: scale(0);
+}
+
+.hu-color-picker {
+    box-shadow: none;
+
+    &.light {
+        background: #2d3748;
+
+        .color-type {
+            .name {
+                background: #4a5568;
+                color: #fff;
+            }
+
+            .value {
+                background: #000;
+                color: #fff;
+            }
+        }
+    }
+
+    .color-show {
+        display: none;
+    }
+
+    &>div:nth-last-child(-2n + 2),
+    .color-alpha {
+        display: none;
+    }
+
+    .color-show {
+        justify-items: flex-start;
+
+        canvas {
+            width: 100% !important;
+        }
+    }
+
+    &>div:last-of-type {
+        .colors {
+            margin: 0;
+            margin-top: 0.5rem;
+            padding: 0;
+            display: grid;
+            grid-template-columns: repeat(5, 30px);
+            grid-column-gap: 0.5rem;
+
+            .item {
+                box-shadow: none;
+                margin: 0;
+                width: auto;
+                height: 30px;
+
+                &:hover {
+                    transform: none;
+                }
+
+                &:nth-last-child(-n + 14) {
+                    display: none;
+                }
+            }
+        }
+
+        .history {
+            display: none;
+        }
+    }
+}</style>
